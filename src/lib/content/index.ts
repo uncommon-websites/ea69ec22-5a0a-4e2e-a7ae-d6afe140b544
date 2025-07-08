9475653c9456c3583daa2e2f48fcedfc88e5404a
Uncle Common <krishnerkar@gmail.com>
2025-07-08 12:43:23 +0000

    Update brand variables and company name

--- src/lib/content/index.ts
+++ src/lib/content/index.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-	companyName: "Everyone who needs a super simple website",
+	companyName: "Ordinary",
 	description: "This company provides a service for individuals and businesses to create super simple websites, focusing on ease of use and accessibility for non-technical users.",
 	image: "/og.png",
 	url: "https://example.com",
